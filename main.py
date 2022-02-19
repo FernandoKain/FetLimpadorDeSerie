7803b4d2a6efb980054ee508797d5de6acf56b63
Fernando Kain <fernandomanga@hotmail.com>
2022-02-19 13:01:13 -0300

    Arrumado alguns comandos

--- main.py
+++ main.py
@@ -60,7 +60,17 @@ def backspace():
 def delete():
     pyautogui.press('delete')
 
+def end():
+    pyautogui.press('end')
 
+def space():
+    pyautogui.press('space')
+
+def tab():
+    pyautogui.press('tab')
+
+def home():
+    pyautogui.press('home')
 
 # -------------Código começa------------------------
 
@@ -68,22 +78,167 @@ def delete():
 alttab()
 time.sleep(0.5)
 
+# Utilizar o localizar primeiro e substituir os itens desnecessário para depois usar o código
+
+# for i in range(12):
+#     for i in range(44):
+#         f2()
+#         up()
+#         up()
+#         delete()
+#         up()
+#         end()
+#         backspace()
+#         enter()
+#     for i in range(44):
+#         up()
+#     right()
+
+
+
+# ----------RoboWord-----------------
+
+
+##----------- RoboWord Noit ------------
+# for i in range(1):
+#     for i in range(35):
+#         end()
+#         backspace()
+#         space()
+#         down()
+#
+#     for i in range(36):
+#         up()
+#
+#     tab()
+#     down()
+#     down()
+
+
+#----------- RoboWord Manhã Tarde------------
+# for i in range(12):
+#     for i in range(44):
+#         end()
+#         backspace()
+#         space()
+#         down()
+#
+#     for i in range(45):
+#         up()
+#
+#     tab()
+#     down()
+
+# for i in range(5):
+#     end()
+#     backspace()
+#     space()
+#     down()
+#
+#     end()
+#     backspace()
+#     space()
+#     down()
+#
+#     end()
+#     backspace()
+#     space()
+#     down()
+#
+#     end()
+#     backspace()
+#     space()
+#     down()
+#
+#     down()
+#
+#     end()
+#     backspace()
+#     space()
+#     down()
+#
+#     end()
+#     backspace()
+#     space()
+#     down()
+#
+#     end()
+#     backspace()
+#     space()
+#     down()
+#
+#     down()
+
+
+
+# Limpador de série 1
+
+# for i in range(12):
+#     for i in range(44):
+#         f2()
+#         up()
+#         up()
+#         up()
+#         backspace()
+#         backspace()
+#         backspace()
+#         delete()
+#         enter()
+#     for i in range(44):
+#         up()
+#     right()
+
+# Limpador de Série 2
+# for i in range(12):
+#     for i in range(44):
+#         f2()
+#         up()
+#         end()
+#         delete()
+#         space()
+#         enter()
+#     for i in range(44):
+#         up()
+#     right()
+
+
+# Limpador de Série 3
 for i in range(12):
     for i in range(44):
         f2()
         up()
         up()
         up()
-        backspace()
-        backspace()
-        backspace()
-        backspace()
+        home()
+        delete()
+        delete()
+        delete()
+        delete()
+        end()
         delete()
         enter()
     for i in range(44):
         up()
     right()
 
+right()
+right()
+right()
 
-
-
+for i in range(12):
+    for i in range(44):
+        f2()
+        up()
+        up()
+        up()
+        home()
+        delete()
+        delete()
+        delete()
+        delete()
+        end()
+        delete()
+        enter()
+    for i in range(44):
+        up()
+    right()
